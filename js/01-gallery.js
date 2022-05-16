d00128553b66f493877b40b8517cb4e8b4f1518f
Tetiana Doroshko <yanisolua@gmail.com>
2022-05-16 16:27:48 +0300

    Adds extra images. Adds lazyloading.

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,12 +1,16 @@
 import { galleryItems } from "./gallery-items.js";
+import { galleryItemsExtra } from "./gallery-items.js";
+
 // Change code below this line
 
 console.log(galleryItems);
+console.log(galleryItemsExtra);
 
 // рендер разметки items
 const galleryRef = document.querySelector(".gallery");
 
 makeGalleryItemsMurkup(galleryItems);
+makeGalleryItemsMurkup(galleryItemsExtra);
 
 function makeGalleryItemsMurkup(arrayItems) {
   const itemsMarkupString = arrayItems
@@ -15,10 +19,11 @@ function makeGalleryItemsMurkup(arrayItems) {
         <div class="gallery__item">
         <a class="gallery__link" href="${item.original}">
             <img
-            class="gallery__image"
-            src="${item.preview}"
+            class="gallery__image lazyload"
+            data-src="${item.preview}"
             data-source="${item.original}"
             alt="${item.description}"
+            loading="lazy"
             />
         </a>
         </div>`;
@@ -51,4 +56,20 @@ function onImageClick(event) {
     instance.close();
     window.removeEventListener("keydown", closeModalImage);
   }
+}
+
+// проверка lazyloading, подключение lazysizes
+
+if ("loading" in HTMLImageElement.prototype) {
+  const images = document.querySelectorAll("img[loading]");
+  images.forEach((image) => (image.src = image.dataset.src));
+} else {
+  const script = document.createElement("script");
+  script.src =
+    "https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js";
+  script.integrity =
+    "sha512-q583ppKrCRc7N5O0n2nzUiJ+suUv7Et1JGels4bXOaMFQcamPk9HjdUknZuuFjBNs7tsMuadge5k9RzdmO+1GQ==";
+  script.crossorigin = "anonymous";
+  script.referrerpolicy = "no-referrer";
+  document.body.append(script);
 }
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,20 +1,24 @@
 import { galleryItems } from "./gallery-items.js";
+import { galleryItemsExtra } from "./gallery-items.js";
+
 // Change code below this line
 
 console.log(galleryItems);
+console.log(galleryItemsExtra);
 
 // рендер разметки items
 
 const galleryRef = document.querySelector(".gallery");
 
 makeGalleryItemsMurkup(galleryItems);
+makeGalleryItemsMurkup(galleryItemsExtra);
 
 function makeGalleryItemsMurkup(arrayItems) {
   const itemsMarkupString = arrayItems
     .map((item) => {
       return `
         <a class="gallery__item" href="${item.original}">
-            <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+            <img class="gallery__image lazyload" loading="lazy" data-src="${item.preview}" alt="${item.description}" />
         </a>`;
     })
     .join("");
@@ -30,4 +34,18 @@ const lightbox = new SimpleLightbox(".gallery a", {
   captionDelay: 250,
 });
 
-galleryRef.addEventListener("click", onImageClick);
+// проверка lazyloading, подключение lazysizes
+
+if ("loading" in HTMLImageElement.prototype) {
+  const images = document.querySelectorAll("img[loading]");
+  images.forEach((image) => (image.src = image.dataset.src));
+} else {
+  const script = document.createElement("script");
+  script.src =
+    "https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js";
+  script.integrity =
+    "sha512-q583ppKrCRc7N5O0n2nzUiJ+suUv7Et1JGels4bXOaMFQcamPk9HjdUknZuuFjBNs7tsMuadge5k9RzdmO+1GQ==";
+  script.crossorigin = "anonymous";
+  script.referrerpolicy = "no-referrer";
+  document.body.append(script);
+}
