18ed61475ab998e83c90e99d16c0b0797eb992cd
Tetiana Doroshko <yanisolua@gmail.com>
2022-05-16 12:29:54 +0300

    Adds resilve task-02.

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,33 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+// рендер разметки items
+
+const galleryRef = document.querySelector(".gallery");
+
+makeGalleryItemsMurkup(galleryItems);
+
+function makeGalleryItemsMurkup(arrayItems) {
+  const itemsMarkupString = arrayItems
+    .map((item) => {
+      return `
+        <a class="gallery__item" href="${item.original}">
+            <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+        </a>`;
+    })
+    .join("");
+  galleryRef.insertAdjacentHTML("beforeend", itemsMarkupString);
+}
+
+// инициализация библиотеки
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
+
+galleryRef.addEventListener("click", onImageClick);
